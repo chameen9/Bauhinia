fe07fabe7794d0610f06c349a01725d2dc35d85f
ChameenS <chameensandeepa9@gmail.com>
2022-07-09 23:11:10 +0530

    09.07.2022

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -62,7 +62,7 @@
                 </li>
                 <!-- Contact -->
                 <li class="nav-item">
-                  <a class="nav-link" href="{{ url('/contact') }}"></a>
+                  <a class="nav-link" href="{{ url('/contact') }}">Contact</a>
                 </li>
               </ul>
 
